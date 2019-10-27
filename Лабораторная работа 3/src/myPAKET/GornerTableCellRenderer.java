aa6efca530d160f8674e9100c0e8e1b2adcc478b
Cronus023 <anlrey.klochko@mail.ru>
2019-10-27 21:34:26 +0300

    In the constructor add label to panel and customize "formatter"

--- Лабораторная работа 3/src/myPAKET/GornerTableCellRenderer.java
+++ Лабораторная работа 3/src/myPAKET/GornerTableCellRenderer.java
@@ -28,6 +28,24 @@ public class GornerTableCellRenderer implements TableCellRenderer {
 	//this field for formatting string representation of numbers
 	private DecimalFormat formatter = (DecimalFormat)NumberFormat.getInstance();
 	
+	public GornerTableCellRenderer() {
+		// show only 5 decimal places
+		formatter.setMaximumFractionDigits(5);
+		
+		// do not use grouping (do not separate thousand commas or spaces)
+		formatter.setGroupingUsed(false);
+		
+		//set a point as the decimal separator
+		DecimalFormatSymbols dottedDouble = formatter.getDecimalFormatSymbols();
+		dottedDouble.setDecimalSeparator('.');
+		formatter.setDecimalFormatSymbols(dottedDouble);
+		
+		panel.add(label);
+		// the left alignment
+		panel.setLayout(new FlowLayout(FlowLayout.LEFT));
+	}
+
+	
 	public Component getTableCellRendererComponent(JTable arg0, Object arg1, boolean arg2, boolean arg3, int arg4,
 			int arg5) {
 		// TODO Auto-generated method stub

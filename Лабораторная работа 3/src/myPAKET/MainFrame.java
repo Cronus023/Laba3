b014e26c637022a6c12ee069cc7755668953e918
Cronus023 <anlrey.klochko@mail.ru>
2019-10-31 19:11:02 +0300

    add some fields for our class and start implementing the main window

--- Лабораторная работа 3/src/myPAKET/MainFrame.java
+++ Лабораторная работа 3/src/myPAKET/MainFrame.java
@@ -1,9 +1,67 @@
 package myPAKET;
 
 import javax.swing.JFrame;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
 
 public class MainFrame extends JFrame {
-
+	// the size of the main window
+	private static final int WIDTH = 700;
+	private static final int HEIGHT = 500;
+	
+	//array of coefficients of the polynomial
+	private Double[] coefficients;
+	
+	// the object dialog box to select files
+	private JFileChooser fileChooser = null;
+	
+	//menu item
+	private JMenuItem saveToTextMenuItem;
+	private JMenuItem saveToGraphicsMenuItem;
+	private JMenuItem searchValueMenuItem;
+	
+	private JTextField textFieldFrom;
+	private JTextField textFieldTo;
+	private JTextField textFieldStep;
+	private Box hBoxResult;
+	
+	private GornerTableCellRenderer renderer = new GornerTableCellRenderer();
+	private GornerTableModel data;
+    
+	public MainFrame(Double[] coefficients) {
+		super("Табулирование многочлена на отрезке по схеме Горнера");
+		this.coefficients = coefficients;
+		setSize(WIDTH, HEIGHT);
+		Toolkit kit = Toolkit.getDefaultToolkit();
+		setLocation((kit.getScreenSize().width - WIDTH)/2,
+		(kit.getScreenSize().height - HEIGHT)/2);
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 

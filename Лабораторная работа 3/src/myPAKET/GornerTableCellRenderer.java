8c4fbade16e42901c6f6479d5fa6926fd94eae33
Cronus023 <anlrey.klochko@mail.ru>
2019-11-21 17:32:42 +0300

    change Component according to the task

--- Лабораторная работа 3/src/myPAKET/GornerTableCellRenderer.java
+++ Лабораторная работа 3/src/myPAKET/GornerTableCellRenderer.java
@@ -10,9 +10,9 @@ import java.awt.FlowLayout;
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
 import java.text.NumberFormat;
+
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTable;
 import javax.swing.table.TableCellRenderer;
 public class GornerTableCellRenderer implements TableCellRenderer {
 	
@@ -20,7 +20,6 @@ public class GornerTableCellRenderer implements TableCellRenderer {
 	for this we use the analogy of finding a needle in hay, in the role of hay-table*/
 	
 	private String needle = null;
-	
 	//our "workhorse"
 	private JPanel panel = new JPanel();
 	private JLabel label = new JLabel();
@@ -28,10 +27,10 @@ public class GornerTableCellRenderer implements TableCellRenderer {
 	//this field for formatting string representation of numbers
 	private DecimalFormat formatter = (DecimalFormat)NumberFormat.getInstance();
 	
-	
 	public void setNeedle(String needle) {
         this.needle = needle;
 	}
+	
 	public GornerTableCellRenderer() {
 		// show only 5 decimal places
 		formatter.setMaximumFractionDigits(5);
@@ -54,14 +53,37 @@ public class GornerTableCellRenderer implements TableCellRenderer {
 			String formattedDouble = formatter.format(value);
 			// set the label text equal to the string representation of a number
 			label.setText(formattedDouble);
+			boolean flag = false ,flag1 = false;
+			if(col!=2) {
+			    Double ValueOfCell = Double.valueOf(formattedDouble).doubleValue();
+			
+			    Double ClonValueOfCell = ValueOfCell;
+			
+		        //check the integer part of the number for parity or odd	
+			    while( ValueOfCell.intValue()!=0) {
+				    if(ValueOfCell.intValue()%2 != 0) {
+					    flag=true;
+				    }
+				    ValueOfCell/=10;
+			    }
+			    //check the fractional part of the number for parity or odd
+			    while(ClonValueOfCell - ClonValueOfCell.intValue()!=0) {
+				    if((ClonValueOfCell*10) % 2 != 0) {
+					    flag1 = true;
+				    }
+				    ClonValueOfCell*=10;
+			    }
+			}
 			if (col==1 && needle!=null && needle.equals(formattedDouble)) {
 			    //paint the cell in red, if we find a needle
 			    panel.setBackground(Color.RED);
 			} 
+			else if(col!=2 && flag == false && flag1 == false){
+			    panel.setBackground(Color.GREEN);    
+			}
 			else {
-			    panel.setBackground(Color.WHITE);
+				panel.setBackground(Color.WHITE);
 			}
 		return panel;
 	}
-	
 }
